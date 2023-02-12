230a4b745074cb199342a421c0a43272ba76e785
splean101 <101splean@gmail.com>
2023-02-12 10:55:01 +0200

    3 done

--- script.js
+++ script.js
@@ -28,4 +28,32 @@ async function getAsyncData(url) {
 getAsyncData(url);
 //--------------------3-----------------------------
 
+const userA = document.querySelector('.user-a');
+const userAB = document.querySelector('.user-ab');
+
+async function getAsyncData1(url) {
+  try {
+    let result = await fetch(url);
+    if (result.ok) {
+      let response = await result.json();
+      let filteredA = response.filter((item) => item.title[0] === 'a');
+      filteredA.forEach((element) => {
+        const el = document.createElement('p');
+        el.textContent = JSON.stringify(element);
+        userA.append(el);
+      });
+      let filteredAB = filteredA.filter((item) => item.title.startsWith('ab'));
+      filteredAB.forEach((element) => {
+        const el = document.createElement('p');
+        el.textContent = JSON.stringify(element);
+        userAB.append(el);
+      });
+    }
+  } catch (err) {
+    console.log(err, err.message);
+  }
+}
+
+getAsyncData1(url);
+
 //--------------------4-----------------------------
