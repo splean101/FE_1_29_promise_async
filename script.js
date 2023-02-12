793b7e50a95ea65497015a1cd651e073f00b618a
splean101 <101splean@gmail.com>
2023-02-12 11:43:35 +0200

    4 done

--- script.js
+++ script.js
@@ -57,3 +57,48 @@ async function getAsyncData1(url) {
 getAsyncData1(url);
 
 //--------------------4-----------------------------
+
+const buttonA = document.querySelector('.add-a');
+const buttonAB = document.querySelector('.add-ab');
+const buttonAll = document.querySelector('.add-all');
+
+async function getAsyncData2(url) {
+  try {
+    let result = await fetch(url);
+    if (result.ok) {
+      let response = await result.json();
+      let filteredA = response.filter((item) => item.title[0] === 'a');
+
+      buttonA.addEventListener('click', addA);
+
+      function addA() {
+        filteredA.forEach((element) => {
+          const el = document.createElement('p');
+          el.textContent = JSON.stringify(element);
+          userA.append(el);
+        });
+      }
+
+      buttonAB.addEventListener('click', addAB);
+
+      let filteredAB = filteredA.filter((item) => item.title.startsWith('ab'));
+
+      function addAB() {
+        filteredAB.forEach((element) => {
+          const el = document.createElement('p');
+          el.textContent = JSON.stringify(element);
+          userAB.append(el);
+        });
+      }
+
+      buttonAll.addEventListener('click', ()=>{
+        addA();
+        addAB()
+      })
+    }
+  } catch (err) {
+    console.log(err, err.message);
+  }
+}
+
+getAsyncData2(url);
